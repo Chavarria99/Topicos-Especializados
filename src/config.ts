79ff879cc05e3526a3fc469b42184ae8fa4c232a
MRichard97 <93174245+MRichard97@users.noreply.github.com>
2022-10-05 12:28:38 -0600

    Primeros archivos del proyecto

---
+++ src/config.ts
@@ -0,0 +1,38 @@
+export const NAV_ITEMS = [
+    {
+      path: '/',
+      title: 'This is Massively',
+    },
+    {
+      path: '/generic',
+      title: 'Generic Page',
+    },
+    {
+      path: '/elements',
+      title: 'Elements Reference',
+    },
+  ];
+  
+  export const SOCIAL_MEDIA_ITEMS = [
+    {
+      path: '#',
+      title: 'Twitter',
+      iconClass: 'fa-twitter',
+    },
+    {
+      path: '#',
+      title: 'Facebook',
+      iconClass: 'fa-facebook-f',
+    },
+    {
+      path: '#',
+      title: 'Instagram',
+      iconClass: 'fa-instagram',
+    },
+    {
+      path: '#',
+      title: 'GitHub',
+      iconClass: 'fa-github',
+    },
+  ];
+  
